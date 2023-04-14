b392b327d2b4133bc445854470ab6b7932efb15c
Tomáš Turek <tom.turek@atlas.cz>
2023-04-14 09:03:56 +0200

    removed cpp

--- cpp/src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ueo pipefail
-
-# This is build for linux, on other OS you may switch : for ;
-javac -cp graphs.jar:. cz/cuni/mff/opt/cpp/*.java
-java -cp graphs.jar:. cz.cuni.mff.opt.cpp.Main
